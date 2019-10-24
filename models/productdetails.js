603cb310b285f708f7134af7bcbe18fd7b9984b0
rheaacharya77 <46256118+rheaacharya77@users.noreply.github.com>
2019-10-24 17:06:06 +0545

    all changes made by Riya

--- models/productdetails.js
+++ models/productdetails.js
@@ -1,15 +1,13 @@
 var mongoose= require('mongoose');
 const productdetailsSchema = mongoose.Schema({
     name: String,
-    email:String,
     price: Number,
-    phone:Number,
-    description: String,
-    image: String,
     condition:String,
+    description: String,
+    image:String,
     CreatedData: {
         type:Date,
         default:Date.now
     }
 });
-module.exports= mongoose.model('productdetails', productdetailsSchema);
+module.exports= mongoose.model('productdetails',productdetailsSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,111 +1,136 @@
 
+// var express = require('express');
+// var router = express.Router();
+// var productdetails = require('../models/productdetails')
+
+
+
+// /* GET home page. */
+
+
+
+
+// router.get('/', function(req, res, next) {
+//   productdetails.find().exec(function(err,productdetails){
+//     console.log(',,,data',productdetails)
+//     res.render('index',{productdetails})
+//   })
+// });
+
+
+
+
+// router.get('/productdetails', function(req, res, next) { 
+//   productdetails.find().exec(function(err,productdetails){
+//     console.log(',,,,,data',productdetails)
+//     res.render('viewdetails',{productdetails})
+//   })
+  
+// });
+
+//  router.get('/adddetails', function(req, res, next) {
+//      res.render('adddetails')
+// });
+
+
+// router.post('/adddetails', function(req, res, next) {
+//   console.log(req.body)
+//   var productdetail = new Productdetails({
+//   name: req.body.name,
+//   price: req.body.price,
+//   condition: req.body.condition,
+//   description: req.body.description,
+  
+// })
+// var promise = productdetail.save()
+// promise.then((productdetails) => {
+//   console.log('product saved',productdetails)
+// })
+// res.redirect('viewdetails');
+// });
+
+// router.get('/productdetail/:id', function(req, res, next) {
+//   Movies.findOne({_id:req.params.id}, function(err,productdetail){
+//     console.log('movie selected....',productdetail)
+//     //res.render('viewOne',{movie})
+// })
+
+
+// })
+
+
+
+
+ 
+
+// module.exports = router;
 var express = require('express');
 var router = express.Router();
-var Productdetails = require('../models/productdetails')
+var productdetails= require('../models/productdetails');
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
 
 
-router.get('/productdetails', function(req, res, next) { 
-  Productdetails.find().exec(function(err,productdetails){
-    console.log(',,,,,data',productdetails)
-    res.render('viewalldetails',{productdetails})
+ router.get('/', function(req, res, next) {
+  productdetails.find().exec(function(err,productdetails){
+    console.log('....data',productdetails)
+   res.render('index',{productdetails})
   })
-  
-});
+});   
 
-
- router.get('/adddetails', function(req, res, next) {
-   //Movies.find().exec(function(err,movies){
-     res.render('adddetails')
-   //})
-  
-});
-
-router.post('/adddetails', function(req, res, next) {
-  console.log(req.body)
-  var productdetail = new Productdetails({
-  name: req.body.name,
-  price: req.body.price,
-  email: req.body.email,
-  phone:req.body.phone,
-  condition: req.body.condition,
-  description: req.body.description,
-  
+router.get('/adddetails',function(req,res,next){
+  res.render('adddetails')
 })
-var promise = productdetail.save()
-promise.then((productdetail) => {
-  console.log('product saved',productdetail)
-  //return res.redirect('movies')
+
+router.get('/viewdetails/:_id',function(req,res,next)
+{
+  productdetails.findOne({_id: req.params._id},function(err,productdetails){
+     console.log('movie selected.....',productdetails)
+    res.render('viewdetails',{productdetails})
+
+ })
+
 })
-res.redirect('/editdelete');
 
-});
 
- router.get('/editdelete', function(req, res, next) {
-   //Movies.find().exec(function(err,movies){
-     res.render('editdelete')
-   //})
+
+
+
+// router.get('/buyproducts', function(req, res, next) { //this part of code is for buyproducts database
+//   buyproducts.find().exec((err,buyproducts)=>{
+//     console.log('movies......',buyproducts)
+//      return buyproducts;
+//   })
+// })
+
+
+// router.get('/productdetails', function(req, res, next) {
+//   res.render('productdetails')
+// });
+// router.post('/productdetails', function (req, res, next) {
+//   console.log(req.body);
+//   //creating a variable for 
+//   var productdetail= new productdetails({
+//     name: req.body.name,
+//     price: req.body.price,
+//     description: req.body.description,
+//     condition: req.body.condition,
+// image:req.body.image,
+//   })
+//   var promise=productdetail.save()
+//   promise.then((productdetails)=>{
+//     console.log('products saved',productdetails)
+//   })
+//   res.redirect('/');
+// })
+
  
-});
-
-
-
-router.get('/editdelete/:_id', function(req, res, next) {
-  Productdetails.findOne({_id:req.params.id}, function(err,productdetail){
-    console.log('movie selected....',productdetail)
-    res.render('editdelete',{productdetail})
-})
-
-
-})
-
-
-
-
-router.get('/productdetail/:id', function(req, res, next) {
-  Productdetails.findOne({_id:req.params.id}, function(err,productdetail){
-    console.log('movie selected....',productdetail)
-    res.render('viewselldetails',{productdetail})
-})
-
-
-})
-
-
-  router.get('/delete/:_id',function(req, res, next) {
-    Productdetails.deleteOne({_id:req.params._id}, function(err,productdetail){
-      console.log('movie deleted.....',productdetail)
-      res.redirect('/productdetails')
-    
-  })
-})
-
-
-router.get('/update/:_id',function(req, res, next) {
-  Productdetails.findOne({_id:req.params._id}, function(err,productdetail){
-    console.log('product deleted.....',productdetail)
-    res.render('updatedetails',{productdetails})
-  
-})
-})
-
-router.post('/update', function(req, res, next) {
-  Productdetails.findOneAndUpdate({_id: req.body._id}, {$set: req.body}, function(err,productdetail){
-    res.redirect('/productdetails');
-  
-})
-});
-
-
-
-
-module.exports = router;
+module.exports = router ;
 
 
 
